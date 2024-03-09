592e8271c35561b30f3cf744415f40df428b4a56
kansuorhan <kansuorhan@gmail.com>
2024-03-09 17:57:10 +0300

    index

---
+++ main.js
@@ -0,0 +1,30 @@
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const repeatButton = document.getElementById("repeat");
+const shuffleButton = document.getElementById("shuffle");
+const audio = document.getElementById("audio");
+const songImage = document.getElementById("song-image");
+const songName = document.getElementById("song-name");
+const songArtist = document.getElementById("song-artist");
+const pauseButton = document.getElementById("pause");
+const playButton = document.getElementById("play");
+const playListButton = document.getElementById("playlist");
+
+const maxDuration = document.getElementById("max-duration");
+const currentTimeRef = document.getElementById("current-time");
+
+const progressBar = document.getElementById("progress-bar");
+const playListContainer = document.getElementById("playlist-container");
+const closeButton = document.getElementById("close-button");
+const playListSongs = document.getElementById("playlist-songs");
+
+const currentProgress = document.getElementById("current-progress");
+
+
+
+window.onload = () => {
+  index = 0
+  setSong(index)
+  pauseAudio()
+  initializePlaylist()
+}
